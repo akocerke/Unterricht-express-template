d1481448fe0951f55a9ef141649612342e5282ee
akocerke <152269613+akocerke@users.noreply.github.com>
2024-03-11 13:52:20 +0100

    update

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const { AppRouter } = require("./src/routes");
+import app from './src/server';
 const todoSequelize = require("./src/database/setup/database");
 
 // Zugriff auf Umgebungsvariablen
 // const { PORT } = process.env;
-
 const PORT = process.env.PORT;
-
-// Initialisierung von expres
-const app = express();
-app.use(bodyParser.json());
-
-// Use for development
-app.use(cors());
 
 todoSequelize
   .sync()
@@ -25,7 +14,7 @@ todoSequelize
     console.log(e);
   });
 
-app.use("/v1", AppRouter);
+
 
 // App hört im folgenden auf den Port, welcher über die Umgebungsvariable definiert ist
 app.listen(PORT, () => {
