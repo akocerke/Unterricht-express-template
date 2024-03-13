10a1692304d48475eca5f4d14b977529f4b6282d
akocerke <152269613+akocerke@users.noreply.github.com>
2024-03-13 09:46:41 +0100

    Update

--- tests/routes/todos/TodosRoute.test.js
+++ tests/routes/todos/TodosRoute.test.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const app = require("../../../src/server");
+const { StatusCodes } = require('http-status-codes');
 
 describe("GET /v1/todos/all", () => {
   test("responds with json", async () => {
@@ -43,5 +44,107 @@ describe("GET /v1/todos/byid", () => {
 
     // Annahme: Der Server gibt ein leeres Objekt zurück, wenn keine ToDo mit der angegebenen ID gefunden wurde
     expect(response.body).toEqual({ todo: null });
+  });
+});
+
+describe("Test Mutations (POST CREATE)", () => {
+  test("Test Create Object", async () => {
+    const response = await request(app)
+      .post(`/v1/todos/create`)
+      .send({
+        newTask: "Tennis spielen",
+        newIsDone: false,
+        newDueDate: "2026-10-10",
+        newUserId: 2,
+      })
+      .expect("Content-Type", /json/)
+      .expect(200);
+  });
+});
+
+
+
+
+describe("Test Mutations (PUT MARK)", () => {
+  test("Test Mark Object as Done", async () => {
+    // Annahme: Eine Aufgabe mit der ID 123 existiert bereits
+    const todoId = 123;
+    const newIsDone = true;
+
+    const response = await request(app)
+      .put(`/v1/todos/mark`)
+      .send({
+        todoId: todoId,
+        newIsDone: newIsDone,
+      })
+      .expect("Content-Type", /json/)
+      .expect(StatusCodes.OK);
+
+    // Überprüfen Sie die Antwort, wenn erforderlich
+    expect(response.body.updatedTodoId).toBe(todoId);
+  });
+  test("Test Mark Object as NOT Done", async () => {
+    // Annahme: Eine Aufgabe mit der ID 123 existiert bereits
+    const todoId = 3;
+    const newIsDone = false;
+
+    const response = await request(app)
+      .put(`/v1/todos/mark`)
+      .send({
+        todoId: todoId,
+        newIsDone: newIsDone,
+      })
+      .expect("Content-Type", /json/)
+      .expect(StatusCodes.OK);
+
+    // Überprüfen Sie die Antwort, wenn erforderlich
+    expect(response.body.updatedTodoId).toBe(todoId);
+  });
+});
+
+
+
+describe("Test Mutations (PUT UPDATE)", () => {
+  test("Test Update Object", async () => {
+    // Annahme: Eine Aufgabe mit der ID 123 existiert bereits
+    const todoId = 3;
+    const newTask = "Neue Aufgabe";
+    const newIsDone = true;
+    const newDueDate = "2024-03-15";
+
+    const response = await request(app)
+      .put(`/v1/todos/update`)
+      .send({
+        todoId: todoId,
+        newTask: newTask,
+        newIsDone: newIsDone,
+        newDueDate: newDueDate,
+      })
+      .expect("Content-Type", /json/)
+      .expect(StatusCodes.OK);
+
+    // Überprüfen Sie die Antwort, wenn erforderlich
+    expect(response.body.updatedTodoId).toBe(todoId);
+  });
+});
+
+
+
+
+describe("Test Mutations (DELETE)", () => {
+  test("Test Delete Object", async () => {
+    // Annahme: Eine Aufgabe mit der ID 123 existiert bereits
+    const todoId = 123;
+
+    const response = await request(app)
+      .delete(`/v1/todos/delete`)
+      .send({
+        todoId: todoId,
+      })
+      .expect("Content-Type", /json/)
+      .expect(StatusCodes.OK);
+
+    // Überprüfen Sie die Antwort, wenn erforderlich
+    expect(response.body.deletedTodosId).toBe(todoId);
   });
 });
